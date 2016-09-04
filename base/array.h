da3b0fa2df7d336638c928cf647f95f9b23befbe
Dev <lodhad@gmail.com>
2016-09-04 11:17:53 -0700

    I think the array is good?!

--- base/array.h
+++ base/array.h
@@ -2,23 +2,24 @@
 #ifndef _ARRAY_H
 #define _ARRAY_H
 #include "stdafx.h"
+#include "exceptions.h"
 namespace base
 {
-	template<class T> class Array
+	template<class T> class Array						//ARC array
 	{
 	private:
 		T* _pointer;									//Pointer to the array
 		size_t* _references;							//Pointer to the int containing the number of references to the pointer
 		size_t* _numElem;
 	public:
-		Array()
+		Array()											//Main empty initializer
 		{
 			_references = new size_t;					//Allocate a new int for the reference counter
 			*_references = 1;							//Set it to 1 (us only)
 			_numElem = new size_t;						//Allocate space for array length
 		}
 
-		Array(size_t numElem)
+		Array(size_t numElem)							//Initializer which allocates certain amount of space
 		{
 			_references = new size_t;					//Allocate a new int for the reference counter
 			*_references = 1;							//Set it to 1 (us only)
@@ -27,14 +28,22 @@ namespace base
 			_pointer = new T[numElem];					//Allocate space for the array, based off the number of elements requested
 		}
 
-		Array(Array<T>& array)
+		Array(Array<T> array)							//Initializer which loads data from another array, keeping into account ARC
 		{
 			_references = array._references;			//Synchronize the reference counter
 			(*_references)++;							//Increment the reference counter
 			_pointer = array._pointer;					//Synchronize the data pointer
 		}
 
-		~Array()
+		Array<T>& operator=(Array<T> array)				//Assignment operator
+		{
+			_references = array._references;			//Copy over reference counter pointer
+			(*_references)++;							//Increment reference counter
+			_pointer = array._pointer;					//Synchronize pointer
+			return *this;
+		}
+
+		~Array()										//Destructor, which does GC (if necessary)
 		{
 			if (_references != NULL)					//Check to make sure we even have a reference counter
 			{
@@ -48,18 +57,31 @@ namespace base
 			}
 		}
 
-		T& operator[](size_t pos)
+		T& operator[](size_t pos)						//Access operator
 		{
-			if (pos >= _numElem)
+			if (pos >= _numElem)						//Check if within the bounds of the array or die
 			{
+				throw base::ArrayIndexOutOfBoundsException("T& base::Array::operator[](size_t) FATAL: Index provided outside bounds of array!");
 			}
 			
 			return _pointer[pos];
 		}
 
-		T* operator()()
+		T* operator()()									//Gives access to the bare pointer; BE CAREFUL. BAD THINGS CAN HAPPEN
 		{
 			return _pointer;
+		}
+
+		Array<T> allocate(size_t num)
+		{
+			Array<T> ret = (*this);						//We will return the old Array; If unused, the old Array will perform the GC
+			(*_references--);
+
+			_references = new size_t;					//Yes, we are overwriting the pointer
+			_numElem = new size_t;						//Yes, we are overwriting the pointer
+			(*_references) = 1;							//Set the default value for the reference counter (us)
+			(*_numElem) = num;							//Set the number of elements in the array
+			_pointer = new T[*_numElem];				//Allocate more space for our pointer
 		}
 	};
 }
